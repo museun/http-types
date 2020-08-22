c19a492ac891d05cfb5d2183d23ea4ba915fa4d4
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2020-08-22 14:30:16 +0200

    Implement AcceptEncoding

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use crate::{Error, Status, StatusCode};
+use std::str::FromStr;
+
 /// Declares unstable items.
 #[doc(hidden)]
 macro_rules! cfg_unstable {
@@ -7,5 +10,26 @@ macro_rules! cfg_unstable {
             #[cfg_attr(feature = "docs", doc(cfg(unstable)))]
             $item
         )*
+    }
+}
+
+/// Parse a weight of the form `q=0.123`.
+pub(crate) fn parse_weight(s: &str) -> crate::Result<f32> {
+    let mut parts = s.split("=");
+    if !matches!(parts.next(), Some("q")) {
+        let mut err = Error::new_adhoc("invalid weight");
+        err.set_status(StatusCode::BadRequest);
+        return Err(err);
+    }
+    match parts.next() {
+        Some(s) => {
+            let weight = f32::from_str(s).status(400)?;
+            Ok(weight)
+        }
+        None => {
+            let mut err = Error::new_adhoc("invalid weight");
+            err.set_status(StatusCode::BadRequest);
+            Err(err)
+        }
     }
 }
